5b79a6dca09e96315a53c528d370c2637506c1fb
Shreyashs98 <104050227+Shreyashs98@users.noreply.github.com>
2023-08-13 07:57:17 +0530

    error fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -37,11 +37,6 @@ const Header = () => {
               </Link>
             </li>
             <li className='nav-item'>
-              <Link className='nav-link' to='/users'>
-                Users
-              </Link>
-            </li>
-            <li className='nav-item'>
               <Link className='nav-link' to='/login'>
                 Login
               </Link>
